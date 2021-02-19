48d69976239354380d1061222988e05af50ca68a
Guillem Segura <hello@guillemsegura.com>
2021-02-19 14:50:52 +0000

    initial code

---
+++ index.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const { cloneDeep } = require("lodash");
+
+/**
+ *
+ */
+class Collection {
+  constructor(data) {
+    this.data = data;
+    this.tree = { id: null, children: cloneDeep(this.data) };
+  }
+
+  group(fieldName) {
+    findLeaves(this.tree).forEach((l) => {
+      const groups = [];
+      l.children.forEach((d) => {
+        const groupFound = groups.find((s) => s.id === d[fieldName]);
+        if (groupFound) {
+          groupFound.children.push(d);
+        } else {
+          groups.push({ id: d[fieldName], children: [d] });
+        }
+      });
+      l.children = groups;
+    });
+
+    return this;
+  }
+
+  compose(fn) {
+    findLeaves(this.tree).forEach((l, i) => {
+      let prev = null;
+      l.children.forEach((d) => {
+        fn(prev, d, l, i);
+        prev = cloneDeep(d);
+      });
+    });
+
+    return this;
+  }
+
+  get() {
+    return findLeaves(this.tree)
+      .map((l) => l.children)
+      .flat();
+  }
+}
+
+const findLeaves = (tree) => {
+  const leaves = [];
+  function _findLeaves(tree) {
+    if (tree.children && tree.children.find((c) => c.children)) {
+      tree.children.forEach((c) => _findLeaves(c));
+    } else {
+      leaves.push(tree);
+    }
+  }
+  _findLeaves(tree);
+  return leaves;
+};
+
+export default Collection;
